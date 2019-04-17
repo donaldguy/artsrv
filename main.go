d02a3910c762e30489b8bffe02a4ac1db62d7348
Donald Guy <fawkes@mit.edu>
2019-04-16 23:30:45 -0600

    ProdReady: request latency/throughput metrics

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"net/http"
 
 	"github.com/donaldguy/artsrv/art"
 	"github.com/donaldguy/artsrv/requests"
 	"github.com/gin-gonic/gin"
+
+	prom "github.com/prometheus/client_golang/prometheus"
+	"go.opencensus.io/exporter/prometheus"
+	"go.opencensus.io/plugin/ochttp"
+	"go.opencensus.io/stats/view"
 )
 
 func setupRouter() *gin.Engine {
@@ -19,6 +26,33 @@ func setupRouter() *gin.Engine {
 	return r
 }
 
+func setupMetrics(r *gin.Engine) {
+	pe, err := prometheus.NewExporter(prometheus.Options{
+		Registry: prom.DefaultGatherer.(*prom.Registry),
+	})
+	if err != nil {
+		panic(fmt.Errorf("Initializing Prometheus: %v", err))
+	}
+
+	view.RegisterExporter(pe)
+
+	err = view.Register(
+		ochttp.ServerRequestCountView,
+		ochttp.ServerRequestBytesView,
+		ochttp.ServerResponseBytesView,
+		ochttp.ServerLatencyView,
+		ochttp.ServerRequestCountByMethod,
+		ochttp.ServerResponseCountByStatusCode,
+	)
+	if err != nil {
+		panic(fmt.Errorf("Registring ochttp metrics: %v", err))
+	}
+
+	r.GET("/metrics", gin.HandlerFunc(func(c *gin.Context) {
+		pe.ServeHTTP(c.Writer, c.Request)
+	}))
+}
+
 func main() {
 	dbPath := flag.String("db", "/data/art.db", "Path where to create or read a BoltDB file")
 	bind := flag.String("bind", ":4444", "a [<host>]:<port> expression for what IP, port to bind to")
@@ -27,6 +61,10 @@ func main() {
 	art.InitBoltStorage(*dbPath)
 
 	r := setupRouter()
+	setupMetrics(r)
 
-	r.Run(*bind)
+	http.ListenAndServe(
+		*bind,
+		&ochttp.Handler{Handler: r},
+	)
 }
